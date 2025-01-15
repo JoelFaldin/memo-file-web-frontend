4abf334e36c9f390cafdd6146e5ad151d94e937b
Joel Faldín <joelfaldin@gmail.com>
2025-01-15 17:00:14 -0300

    feat: adding temporal form template to send memo data to backend

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,21 @@
 
 import { ref } from 'vue'
 
+const tipo = ref(null)
+const patente = ref(null)
+const rut = ref(null)
+const nombre = ref(null)
+const calle = ref(null)
+const numero = ref(null)
+const aclaratoria = ref(null)
+const periodo = ref(null)
+const capital = ref(null)
+const afecto = ref(null)
+const fechaPagos = ref(null)
+const giro = ref(null)
+const agtp = ref(null)
+
+
 const excel = ref(null)
 
 const handleSubmit = async (e) => {
@@ -11,7 +26,7 @@ const handleSubmit = async (e) => {
   formData.append('excel', excel.value as unknown as Blob)
 
   try {
-    const res = await fetch('http://localhost:3000/excel', {
+    const res = await fetch('http://localhost:3000/excel/upload', {
       method: 'POST',
       body: formData
     })
@@ -23,21 +38,101 @@ const handleSubmit = async (e) => {
   }
 }
 
-const storeFile = (event) => {
-  excel.value = event.target.files[0]
+const storeFile = (event: Event) => {
+    excel.value = event.target.files[0]
+}
+
+const handleSubmitData = async (event: Event) => {
+  event.preventDefault()
+
+  try {
+    const res = await fetch('http://localhost:3000/memo/create', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        tipo: tipo.value,
+        patente: patente.value,
+        rut: rut.value,
+        nombre: nombre.value,
+        calle: calle.value,
+        numero: numero.value,
+        aclaratoria: aclaratoria?.value,
+        periodo: periodo.value,
+        capital: capital.value,
+        afecto: afecto.value,
+        fechaPagos: fechaPagos.value,
+        giro: giro.value,
+        agtp: agtp.value
+      })
+    })
+
+    console.log(res)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 </script>
 
 <template>
-  <div>
+  <div class="excel">
     <label for="file">Seleccione el archivo excel:</label>
     <input type="file" id="file" accept=".xls, .xlsx" @change="storeFile" />
 
     <button @click="handleSubmit">Enviar</button>
   </div>
+
+  <br />
+
+  <form class="form">
+    <label for="tipo">Tipo</label>
+    <input id="tipo" v-model="tipo" />
+
+    <label for="patente">Patente</label>
+    <input id="patente" v-model="patente" />
+
+    <label for="rut">Rut</label>
+    <input id="rut" v-model="rut" />
+
+    <label for="nombre">Nombre</label>
+    <input id="nombre" v-model="nombre" />
+
+    <label for="calle">Calle</label>
+    <input id="calle" v-model="calle" />
+
+    <label for="numero">Numero</label>
+    <input id="numero" v-model="numero" />
+
+    <label for="aclaratoria">Aclaratoria</label>
+    <input id="aclaratoria" v-model="aclaratoria" />
+
+    <label for="periodo">Periodo</label>
+    <input id="periodo" v-model="periodo" />
+    
+    <label for="capital">Capital</label>
+    <input id="capital" v-model="capital" />
+
+    <label for="afecto">Afecto</label>
+    <input id="afecto" v-model="afecto" />
+
+    <label for="fechaPagos">Fecha de pago</label>
+    <input id="fechaPagos" v-model="fechaPagos" />
+
+    <label for="giro">Giro</label>
+    <input id="giro" v-model="giro" />
+
+    <label for="agtp">Agtp</label>
+    <input id="agtp" v-model="agtp" />
+
+    <button @click="handleSubmitData">Enviar datos</button>
+  </form>
 </template>
 
 <style scoped>
-
+  .excel, .form {
+    display: flex;
+    flex-direction: column;
+  }
 </style>
