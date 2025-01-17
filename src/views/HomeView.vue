e930f26628135b40b313808a77f0e7f7a96d91a2
Joel Faldín <joelfaldin@gmail.com>
2025-01-17 16:40:58 -0300

    feat: building main page with overall data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,21 +13,26 @@ const { isLoading, isError, data, error } = useQuery('overall', fetchData);
 </script>
 
 <template>
-    
-    <span v-if="isLoading">
-        <p>Loading...</p>
-    </span>
-    <span v-else-if="isError">
-        <p>There was an error mate</p>
-    </span>
-    <span v-else-if="data">
-        <table>
-            <tr v-for="count in data.totalCount">
-                <td>{{ count.count }}</td>
-                <td>{{ count.label }}</td>
-            </tr>
-        </table>
-    </span>
+    <div class="min-h-screen flex items-center justify-center p-4">
+        <div :class="`flex flex-col items-center bg-card rounded-lg border border-slate-700 p-6 shadow-sm max-w-md w-full`">
+            <h3 class="text-3xl font-bold">Resumen general</h3>
+            <p class="pb-10 pt-2 text-slate-400">Memorándums 2010-2024</p>
 
-
+            <span v-if="isLoading">
+                <p>Cargando tabla resumen...</p>
+            </span>
+            <span v-else-if="isError">
+                <p>Hubo un errorr</p>
+            </span>
+            <span v-else-if="data" class="w-9/12">
+                <table style="width:100%;" class="text-slate-400 mb-2">
+                    <tr v-for="count in data.totalCount"  class="border border-slate-800 hover:bg-slate-700">
+                        <td class="p-2">{{ count.count }}</td>
+                        <td class="p-2" style="width:30%;"></td>
+                        <td class="text-right p-2">{{ count.label }}</td>
+                    </tr>
+                </table>
+            </span>
+            </div>
+        </div>
 </template>
