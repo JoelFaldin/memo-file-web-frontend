cd8627752ddeb7fe63169908b4e785c29b9dac19
Joel Faldín <joelfaldin@gmail.com>
2025-01-20 10:35:40 -0300

    feat: adding table with data on search section

---
+++ src/components/MemoTable.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import { Table, TableCaption, TableHeader, TableRow, TableHead, TableBody,TableCell } from './ui/table';
+
+const headers = ["Memo Id", "Rut", "Tipo", "Patente", "Periodo", "Capital", "Afecto", "Total", "Emisión", "Giro", "AGTP"];
+
+const props = defineProps(['data']);
+console.log(props.data.findMemo)
+</script>
+
+<template>
+    <Table>
+        <TableCaption>Lista de memos</TableCaption>
+        <TableHeader>
+            <TableRow>
+                <TableHead v-for="header in headers">
+                    {{ header }}
+                </TableHead>
+            </TableRow>
+        </TableHeader>
+
+        <TableBody>
+            <TableRow v-for="row in props.data.findMemo">
+                <TableCell>{{ row.id }}</TableCell>
+                <TableCell>{{ row.rut }}</TableCell>
+                <TableCell>{{ row.tipo }}</TableCell>
+                <TableCell>{{ row.patente }}</TableCell>
+                <TableCell>{{ row.periodo }}</TableCell>
+                <TableCell>{{ parseFloat(row.capital) }}</TableCell>
+                <TableCell>{{ row.afecto }}</TableCell>
+                <TableCell>{{ row.total }}</TableCell>
+                <TableCell>{{ row.emision }}</TableCell>
+                <TableCell>{{ row.giro }}</TableCell>
+                <TableCell>{{ row.agtp }}</TableCell>
+            </TableRow>
+        </TableBody>
+    </Table>
+</template>
--- src/views/UpdateMemo.vue
+++ src/views/UpdateMemo.vue
@@ -3,13 +3,14 @@ import { useQuery } from '@tanstack/vue-query';
 import { Label } from 'radix-vue';
 import { ref } from 'vue';
 
+import MemoTable from '@/components/MemoTable.vue';
+
 const patente = ref('');
 
 const getMemos = async (patente: string) => {
   const res = await fetch(`http://localhost:3000/memo/${patente}`);
   const response = await res.json();
 
-  console.log(response);
   return response;
 }
 
@@ -35,8 +36,8 @@ const searchMemo = async (event: Event) => {
 </script>
 
 <template>
-  <div class="min-h-screen flex items-start justify-center p-4">
-    <div  class="flex flex-col items-center bg-card rounded-lg border border-slate-700 p-6 shadow-sm max-w-md w-full">
+  <div class="min-h-screen flex flex-col gap-y-10 items-center justify-start p-4">
+    <section  class="flex flex-col items-center bg-card rounded-lg border border-slate-700 p-6 shadow-sm max-w-md w-full">
       <Label class="text-[15px] font-semibold leading-[35px] text-white">
         Buscar memorándum (por patente):
       </Label>
@@ -49,9 +50,11 @@ const searchMemo = async (event: Event) => {
       <button :class="`h-[35px] mt-5 inline-flex items-center rounded-md ${isLoading ? 'bg-slate-500' : 'bg-indigo-600'} px-3 py-2 text-sm font-semibold text-white shadow-sm ${isLoading ? 'hover:bg-slate-500' : 'hover:bg-indigo-500'} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ${isLoading ? 'focus-visible:outline-slate-500' : 'focus-visible:outline-indigo-600'}`" @click="searchMemo" :disabled="isLoading" >
         Buscar
       </button>
-    </div>
+    </section>
 
-
+    <section v-if="data">
+      <MemoTable :data="data" />
+    </section>
   </div>
 </template>
 
