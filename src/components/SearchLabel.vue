aa3b5fac61955befd8974b1f0f72db478771f501
Joel Faldín <joelfaldin@gmail.com>
2025-01-23 13:28:35 -0300

    refactor: moving input boxes on searchMemo into reusable components

---
+++ src/components/SearchLabel.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const props = defineProps({
+    param: String
+})
+
+defineEmits([
+    `update:param`
+])
+</script>
+
+<template>
+    <span class="flex flex-col">
+        <Label class="text-[15px] font-semibold leading-[35px] text-black dark:text-white">
+            <slot />
+        </Label>
+        <input
+            class="inline-flex h-[35px] w-[200px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px_black] selection:color-white"
+            type="text"
+            :value="props.param"
+            @input="$emit(`update:param`, ($event.target as HTMLInputElement).value)"
+        >
+    </span>
+</template>
--- src/views/SearchMemo.vue
+++ src/views/SearchMemo.vue
@@ -3,6 +3,7 @@ import { useQuery } from '@tanstack/vue-query';
 import { Label } from 'radix-vue';
 import { ref } from 'vue';
 
+import SearchLabel from '@/components/SearchLabel.vue';
 import MemoTable from '@/components/MemoTable.vue';
 import { getMemos } from '@/api/memoService.ts';
 
@@ -38,39 +39,17 @@ const searchMemo = async () => {
   <div class="min-h-screen flex flex-col gap-y-10 items-center justify-start p-4">
     <div class="flex flex-col justify-center items-center gap-y-2">
       <section  class="flex flex-row gap-x-10 items-center justify-center bg-white dark:bg-inherit rounded-lg border border-slate-700 p-6 shadow-sm w-fit">
-        <span class="flex flex-col">
-          <Label class="text-[15px] font-semibold leading-[35px] text-black dark:text-white">
-            Buscar por rol (patente):
-          </Label>
-          <input
-            class="inline-flex h-[35px] w-[200px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px_black] selection:color-white"
-            type="text"
-            v-model="rol"
-          >
-        </span>
-  
-        <span class="flex flex-col">
-          <Label class="text-[15px] font-semibold leading-[35px] text-black dark:text-white">
-            Buscar por rut:
-          </Label>
-          <input
-            class="inline-flex h-[35px] w-[200px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px_black] selection:color-white"
-            type="text"
-            v-model="rut"
-            placeholder="12.345.678-9"
-          >
-        </span>
-  
-        <span class="flex flex-col">
-          <Label class="text-[15px] font-semibold leading-[35px] text-black dark:text-white">
-            Buscar por dirección:
-          </Label>
-          <input
-            class="inline-flex h-[35px] w-[200px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px_black] selection:color-white"
-            type="text"
-            v-model="direction"
-          >
-        </span>
+        <SearchLabel v-model:param="rol">
+          Buscar por rol (patente):
+        </SearchLabel>
+
+        <SearchLabel v-model:param="rut">
+          Buscar por rut:
+        </SearchLabel>
+
+        <SearchLabel v-model:param="direction">
+          Buscar por dirección:
+        </SearchLabel>
         
       </section>
       <button :class="`h-[35px] inline-flex items-center rounded-md w-fit ${isLoading ? 'bg-slate-500' : 'bg-indigo-600'} px-3 py-2 text-sm font-semibold text-white shadow-sm ${isLoading ? 'hover:bg-slate-500' : 'hover:bg-indigo-500'} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ${isLoading ? 'focus-visible:outline-slate-500' : 'focus-visible:outline-indigo-600'}`" @click="searchMemo" :disabled="isLoading" >
