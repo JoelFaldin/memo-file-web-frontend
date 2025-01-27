971909947a54d6a5dd1278b661a37091f7ca63ba
Joel Faldín <joelfaldin@gmail.com>
2025-01-27 15:28:19 -0300

    feat: adding tests for format currency util

--- src/tests/composables/stringUtils.test.ts
+++ src/tests/composables/stringUtils.test.ts
@@ -1,5 +1,7 @@
-import { fixStringLength } from "@/composables/stringUtils/fixStringLength";
 import { describe, expect, test } from 'vitest';
+
+import { fixStringLength } from "@/composables/stringUtils/fixStringLength";
+import { formatCurrency } from "@/composables/stringUtils/formatCurrency";
 
 describe('fix string length util', () => {
     test('removes whitespaces and adds alternative title when strings length is bigger than 15', () => {
@@ -12,3 +14,20 @@ describe('fix string length util', () => {
         expect(fixStringLength('This is a test').fullText).toBe('');
     })
 });
+
+describe('format currency util', () => {
+    test('doesnt format 3 or less digit numbers', () => {
+        expect(formatCurrency(5)).toBe('5');
+        expect(formatCurrency(23)).toBe('23');
+        expect(formatCurrency(456)).toBe('456');
+        expect(formatCurrency(999)).toBe('999');
+    })
+
+    test('formats 4 or more digit numbers', () => {
+        expect(formatCurrency(1000)).toBe('1.000');
+        expect(formatCurrency(1001)).toBe('1.001');
+        expect(formatCurrency(23500)).toBe('23.500');
+        expect(formatCurrency(650000)).toBe('650.000');
+        expect(formatCurrency(6623000)).toBe('6.623.000');
+    })
+})
