c4b00218f6432d5ab729ad38836ccd2252e4fe4f
Joel Faldín <joelfaldin@gmail.com>
2025-01-30 13:28:31 -0300

    feat: improving upload excel page

--- src/views/UploadExcel.vue
+++ src/views/UploadExcel.vue
@@ -39,8 +39,8 @@ const downloadTemplate = async () => {
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col gap-y-5 items-center justify-center">
-    <div class="flex flex-col items-center bg-card rounded-lg bg-white dark:bg-inherit border border-slate-300 dark:border-slate-700 p-6 shadow-sm max-w-md w-full">
+  <div class="flex flex-row items-center justify-center h-screen gap-x-10">
+    <div class="flex flex-col items-center bg-card rounded-lg bg-white dark:bg-inherit border border-slate-300 dark:border-slate-700 p-6 shadow-sm max-w-md w-full h-72">
       <h1 class="text-3xl font-bold text-black dark:text-white">Subir archivo excel</h1>
       <p class="text-slate-700 dark:text-slate-300 m-auto mt-3 text-sm underline underline-offset-4">Importante: el formato de fecha es 20201201 (yyyy-mm-dd)!</p>
 
@@ -66,9 +66,11 @@ const downloadTemplate = async () => {
       </Button>
     </div>
 
-    <div class="flex flex-row items-center gap-x-5">
-      <p class="text-slate-500 my-4">Descarga una plantilla aquí:</p>
+    <div class="flex flex-col items-center bg-card rounded-lg bg-white dark:bg-inherit border border-slate-300 dark:border-slate-700 p-6 shadow-sm max-w-md w-full h-72">
+      <h2 class="text-3xl font-bold text-black dark:text-white">Descarga la plantilla</h2>
+      <p class="text-slate-700 dark:text-slate-300 mt-3 text-sm underline underline-offset-4">Asegúrate que la información esté correctamente formateada</p>
       <Button
+          class="my-auto"
           variant="outline"
           size="lg"
           :disabled="isLoading"
@@ -78,7 +80,7 @@ const downloadTemplate = async () => {
             Descargar plantilla
           </span>
       </Button>
-    </div>
+  </div>
 
     <span v-if="isError">
       {{ error }}
