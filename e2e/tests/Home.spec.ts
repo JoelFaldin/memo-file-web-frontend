630f9573128b5f90cdfb3ef44d339b4d0cb941eb
JoelFaldin <joelfaldin@gmail.com>
2025-03-11 18:26:17 -0300

    feat: adding e2e test to check if table labels are visible

--- e2e/tests/Home.spec.ts
+++ e2e/tests/Home.spec.ts
@@ -13,4 +13,13 @@ test.describe('Home page', () => {
         await expect(page.locator('h3')).toHaveText('Resumen general');
         await expect(page.locator('p')).toHaveText('Lista de memorándums 2010-2024');
     });
+
+    test('renders count of each table', async ({ page }) => {
+        await page.goto('http://localhost:5173');
+
+        await expect(page.locator('td').getByText('Memorándums')).toBeVisible();
+        await expect(page.locator('td').getByText('Fechas de pago')).toBeVisible();
+        await expect(page.locator('td').getByText('Locales únicos')).toBeVisible();
+        await expect(page.locator('td').getByText('Representantes únicos')).toBeVisible();
+    })
 })
