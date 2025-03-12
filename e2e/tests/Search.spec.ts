4ae0f943b66f5e5f3e23b5ff9039523f7c6def9f
JoelFaldin <joelfaldin@gmail.com>
2025-03-12 01:22:27 -0300

    feat: adding search by rut, direction, and loading tests for search page

--- e2e/tests/Search.spec.ts
+++ e2e/tests/Search.spec.ts
@@ -11,6 +11,13 @@ test.describe('Search page', () => {
         await page.getByRole('button', { name: 'Buscar', exact: true }).click();
 
         await expect(page.getByText('Debes indicar un rol, rut o dirección para buscar!')).toBeVisible();
+    });
+
+    test('shows loading notification when loading', async ({ page }) => {
+        await page.fill('input[name="rol"]', '400111-1');
+        await page.getByRole('button', { name: 'Buscar', exact: true }).click();
+
+        await expect(page.getByText('Buscando memorándums...')).toBeVisible();
     })
 
     test('can search by rol', async ({ page }) => {
@@ -27,5 +34,37 @@ test.describe('Search page', () => {
 
         await expect(page.getByText('400111-1')).toBeVisible();
         await expect(page.getByText('Memorándums encontrados!')).toBeVisible();
-    })
+    });
+
+    test('can search by rut', async ({ page }) => {
+        await page.fill('input[name="rut"]', '11.111.111-1');
+        await page.getByRole('button', { name: 'Buscar', exact: true }).click();
+
+        await page.route('**/memo/find?rol=&rut=11111111-1&direction=&page=1&limit=10', async (route) => {
+            await route.fulfill({
+                status: 200,
+                contentType: 'application/json',
+                body: JSON.stringify(searchResponse)
+            });
+        });
+
+        await expect(page.getByText('400111-1')).toBeVisible();
+        await expect(page.getByText('Memorándums encontrados!')).toBeVisible();
+    });
+
+    test('can search by direction', async ({ page }) => {
+        await page.fill('input[name="direction"]', 'random');
+        await page.getByRole('button', { name: 'Buscar', exact: true }).click();
+
+        await page.route('**/memo/find?rol=&rut=&direction=random&page=1&limit=10', async (route) => {
+            await route.fulfill({
+                status: 200,
+                contentType: 'application/json',
+                body: JSON.stringify(searchResponse)
+            });
+        });
+
+        await expect(page.getByText('400111-1')).toBeVisible();
+        await expect(page.getByText('Memorándums encontrados!')).toBeVisible();
+    });
 })
