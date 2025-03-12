44359083a9b2130bc9b56f8c80871e9d582156e6
JoelFaldin <joelfaldin@gmail.com>
2025-03-12 01:35:20 -0300

    feat: adding test to check error notification on search page

--- e2e/tests/Search.spec.ts
+++ e2e/tests/Search.spec.ts
@@ -67,4 +67,23 @@ test.describe('Search page', () => {
         await expect(page.getByText('400111-1')).toBeVisible();
         await expect(page.getByText('Memorándums encontrados!')).toBeVisible();
     });
+
+    test('shows error notification when no results', async ({ page }) => {
+        await page.fill('input[name="rol"]', 'wrong');
+        await page.getByRole('button', { name: 'Buscar', exact: true }).click();
+
+        await page.route('**/memo/find?rol=wrong&rut=&direction=&page=1&limit=10', async (route) => {
+            await route.fulfill({
+                status: 400,
+                contentType: 'application/json',
+                body: JSON.stringify({
+                    "statusCode": 400,
+                    "message": "No se ha encontrado ningún memo con los datos ingresados."
+                })
+            });
+        });
+
+        await page.waitForTimeout(10000);
+        await expect(page.getByText('Ha ocurrido un error al intentar buscar los memorándums. Inténtalo más tarde.')).toBeVisible();
+    })
 })
